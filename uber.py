f7cc2044861c1d9c3e9a21b66c23eb7ada844569
PavithraM999 <60419869+PavithraM999@users.noreply.github.com>
2020-01-29 12:37:58 +0530

    uber billing
    1:Get a destination from the user as an input.
    2:Assign any three destinations in a seperate variable for each destinations.
    3:Fix the prices for the assigned destinations.
    4:Use If-Elif statements to print the prices of the destinations given by the user respectively.

---
+++ uber.py
@@ -0,0 +1,12 @@
+z=input("enter the destination")
+m="avadi"
+n="poonamalle"
+o="porur"
+if(z==m):
+    print("Rs 100")
+elif(z==n):
+    print("Rs 150")
+elif(z==o):
+    print("Rs 200")
+    
+    
